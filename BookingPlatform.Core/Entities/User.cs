a913a0f898ec87464fe0bf513ae09c363829740e
ayabaara <ayabaara4@gmail.com>
2025-06-22 13:40:10 +0200

    Add user and role entities , userRole enum

---
+++ BookingPlatform.Core/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿namespace BookingPlatform.Core.Entities;
+
+public class User : BaseEntity
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public int PhoneNumber { get; set; }
+    public string Password { get; set; } = string.Empty;
+    public DateTime DateOfBirth { get; set; }
+    public Role Role { get; set; } = new Role();
+
+}
+
---
+++ BookingPlatform.Core/Enums/UserRole.cs
@@ -0,0 +1,8 @@
+﻿namespace BookingPlatform.Core.Enums;
+
+public enum UserRole
+{
+    User = 0,
+    Admin = 1
+}
+
