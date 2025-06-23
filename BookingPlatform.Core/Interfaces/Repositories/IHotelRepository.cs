46d82aa57ab814c6116cf85aa0e2e5a65565a5d0
ayabaara <ayabaara4@gmail.com>
2025-06-23 15:39:21 +0200

    add IHotelRepository interface for hotel data operations

---
+++ BookingPlatform.Core/Interfaces/Repositories/IHotelRepository.cs
@@ -0,0 +1,14 @@
+﻿using BookingPlatform.Core.Entities;
+
+namespace BookingPlatform.Core.Interfaces.Repositories;
+
+public interface IHotelRepository
+{
+    Task CreateHotelAsync(Hotel hotel);
+    Task<Hotel> GetHotelByIdAsync(Guid hotelId);
+    Task<PaginatedResult<Hotel>> GetHotelsByCityIdAsync(Guid cityId, int page, int size);
+    Task UpdateHotelAsync(Hotel hotel);
+    Task DeleteHotelByIdAsync(Guid hotelId);
+    Task UpdateHotelRateAsync(Guid hotelId, double newRate);
+}
+
