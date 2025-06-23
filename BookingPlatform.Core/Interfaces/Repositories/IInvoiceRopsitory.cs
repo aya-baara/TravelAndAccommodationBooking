c63ab81f6404e27946f73fb692b78367ac4b5844
ayabaara <ayabaara4@gmail.com>
2025-06-23 15:40:29 +0200

    add IInvoicelRepository interface for invoice data operations

---
+++ BookingPlatform.Core/Interfaces/Repositories/IInvoiceRopsitory.cs
@@ -0,0 +1,14 @@
+﻿using BookingPlatform.Core.Entities;
+
+namespace BookingPlatform.Core.Interfaces.Repositories;
+
+interface IInvoiceRopsitory
+{
+    Task CreateInvoiceAsync(Invoice invoice);
+    Task<Invoice> GetInvoiceByIdAsync(Guid invoiceId);
+    Task<Invoice> GetInvoiceByBookingIdAsync(Guid bookingId);
+    Task UpdateInvoiceAsync(Invoice invoice);
+    Task DeleteInvoiceAsync(Guid invoiceId);
+}
+
+
