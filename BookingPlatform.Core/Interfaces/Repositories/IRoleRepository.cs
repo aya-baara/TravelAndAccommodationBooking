0cec26e7e30a51f125968fd521ac90a92faff9bb
ayabaara <ayabaara4@gmail.com>
2025-06-23 15:41:46 +0200

    add IRoleRepository interface for role data operations

---
+++ BookingPlatform.Core/Interfaces/Repositories/IRoleRepository.cs
@@ -0,0 +1,12 @@
+﻿using BookingPlatform.Core.Entities;
+
+namespace BookingPlatform.Core.Interfaces.Repositories;
+
+public interface IRoleRepository
+{
+    Task CreateRoleAsync(Role role);
+    Task<List<Role>> GetRolesAsync();
+    Task<List<User>> GetAdmins();
+    Task DeleteRoleAsync(Guid roleId);
+}
+
