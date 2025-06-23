a0f830869f4ccca8ea576737feceb13bb3378091
ayabaara <ayabaara4@gmail.com>
2025-06-23 10:23:57 +0200

    add the discount and invoice entities

---
+++ BookingPlatform.Core/Entities/Discount.cs
@@ -0,0 +1,11 @@
+﻿namespace BookingPlatform.Core.Entities;
+
+public class Discount : BaseEntity
+{
+    public Room Room { get; set; }
+    public Guid RoomId { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public decimal Percentage { get; set; }
+}
+
