698b976a7c585feb16ae9da0d004096ec6882cb3
ayabaara <ayabaara4@gmail.com>
2025-06-23 10:21:41 +0200

    add city,hotel,room entities and make relations

---
+++ BookingPlatform.Core/Entities/City.cs
@@ -0,0 +1,13 @@
+﻿namespace BookingPlatform.Core.Entities;
+
+public class City : BaseEntity
+{
+    public string Name { get; set; } = string.Empty;
+    public string Country { get; set; } = string.Empty;
+    public string PostOffice { get; set; } = string.Empty;
+    public String Description { get; set; } = string.Empty;
+    public List<Hotel> Hotels { get; set; } = new List<Hotel>();
+    public DateTime CreateAt { get; set; }
+    public DateTime ModifiedAt { get; set; }
+}
+
---
+++ BookingPlatform.Core/Entities/Hotel.cs
@@ -0,0 +1,26 @@
+﻿
+namespace BookingPlatform.Core.Entities;
+
+public class Hotel : BaseEntity
+{
+    public string Name { get; set; }
+    public string Location { get; set; }
+    public City City { get; set; }
+    public Guid CityId { get; set; }
+    public int StarRating { get; set; }
+    public double ReviewRating { get; set; }
+    public string FullDescription { get; set; }
+    public string BriefDescription { get; set; }
+    public int PhoneNumber { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public Owner Owner { get; set; }
+    public Guid OwnerId { get; set; }
+    public List<Room> Rooms { get; set; } = new List<Room>();
+    public List<Review> Reviews { get; set; } = new List<Review>();
+    public List<Image> Images { get; set; } = new List<Image>();
+    public Image? Thumbnail { get; set; }
+    public DateTime CreateAt { get; set; }
+    public DateTime ModifiedAt { get; set; }
+
+}
