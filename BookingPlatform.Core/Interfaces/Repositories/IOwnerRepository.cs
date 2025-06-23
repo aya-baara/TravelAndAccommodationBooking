d069df88fb80b6fe7b0318bb09df302ad490861c
ayabaara <ayabaara4@gmail.com>
2025-06-23 15:40:58 +0200

    add IOwnerRepository interface for owner data operations

---
+++ BookingPlatform.Core/Interfaces/Repositories/IOwnerRepository.cs
@@ -0,0 +1,13 @@
+﻿using BookingPlatform.Core.Entities;
+
+namespace BookingPlatform.Core.Interfaces.Repositories;
+
+public interface IOwnerRepository
+{
+    Task CreateOwnerAsync(Owner owner);
+    Task<Owner?> GetOwnerByIdAsync(Guid ownerId);
+    Task<PaginatedResult<Hotel>> GetHotelsByOwnerIdAsync(Guid ownerId, int page, int size);
+    Task UpdateOwner(Owner owner);
+    Task DeleteOwnerById(Guid ownerId);
+}
+
