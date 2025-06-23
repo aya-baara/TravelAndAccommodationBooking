1b3ccb0f8152ab29de7d732dcb14a5d32a33a708
ayabaara <ayabaara4@gmail.com>
2025-06-23 10:24:37 +0200

    add the review , Image entities

---
+++ BookingPlatform.Core/Entities/Image.cs
@@ -0,0 +1,14 @@
+﻿using BookingPlatform.Core.Enums;
+
+namespace BookingPlatform.Core.Entities;
+
+public class Image : BaseEntity
+{
+    public ImageType Type { get; set; }
+    public string Path { get; set; }
+    public Guid? HotelId { get; set; }
+    public Hotel? Hotel { get; set; }
+    public Guid? RoomId { get; set; }
+    public Room? Room { get; set; }
+}
+
