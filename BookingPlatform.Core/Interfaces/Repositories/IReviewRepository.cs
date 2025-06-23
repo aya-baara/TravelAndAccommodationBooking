7b0b615c5f78e28acc7597a3f9ba1d8cc04f34ba
ayabaara <ayabaara4@gmail.com>
2025-06-23 15:41:21 +0200

    add IReviewRepository interface for review data operations

---
+++ BookingPlatform.Core/Interfaces/Repositories/IReviewRepository.cs
@@ -0,0 +1,12 @@
+﻿using BookingPlatform.Core.Entities;
+
+namespace BookingPlatform.Core.Interfaces.Repositories;
+
+public interface IReviewRepository
+{
+    Task CreateReviewAsync(Review review);
+    Task<Review?> GetReviewByIdAsync(Guid reviewId);
+    Task<PaginatedResult<Review>> GetReviewsByHotelIdAsync(Guid hotelId, int page, int size);
+    Task UpdateReviewAsync(Review review);
+    Task DeleteReviewById(Guid reviewId);
+}
