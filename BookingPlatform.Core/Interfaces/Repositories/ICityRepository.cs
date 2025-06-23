2dde1f2b19e8f8db27b956c6c7a5b55aa883debd
ayabaara <ayabaara4@gmail.com>
2025-06-23 15:37:18 +0200

    add ICityRepository interface for city data operations

---
+++ BookingPlatform.Core/Interfaces/Repositories/ICityRepository.cs
@@ -0,0 +1,15 @@
+﻿using BookingPlatform.Core.Entities;
+
+namespace BookingPlatform.Core.Interfaces.Repositories;
+
+public interface ICityRepository
+{
+    Task CreateCityAsync(City city);
+    Task<City> GetCityByIdAsync(Guid cityId);
+    Task<City> GetCityByNameAsync(string name);
+    Task UpdateCityAsync(City city);
+    Task DeleteCityAsync(Guid cityId);
+    Task<List<City>> GetTopBookedCitiesAsync(int num);
+
+}
+
