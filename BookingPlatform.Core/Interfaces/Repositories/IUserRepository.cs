7512453f0e69822361e5d636f3b3c73b39f9539e
ayabaara <ayabaara4@gmail.com>
2025-06-23 15:42:44 +0200

    add IUserRepository interface for user data operations

---
+++ BookingPlatform.Core/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using BookingPlatform.Core.Entities;
+
+namespace BookingPlatform.Core.Interfaces.Repositories;
+
+public interface IUserRepository
+{
+    Task<User?> AuthenticateUserAsync(string email, string password);
+    Task CreateUserAsync(User user);
+    Task<User?> GetUserByIdAsync(Guid id);
+    Task<bool> DoesUserExistAsync(string email);
+}
+
