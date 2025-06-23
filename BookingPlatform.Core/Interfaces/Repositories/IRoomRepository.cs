998b9958af24cf8214a41c0c849652df83bc8401
ayabaara <ayabaara4@gmail.com>
2025-06-23 15:42:06 +0200

    add IRoomRepository interface for room data operations

---
+++ BookingPlatform.Core/Interfaces/Repositories/IRoomRepository.cs
@@ -0,0 +1,15 @@
+﻿using BookingPlatform.Core.Entities;
+
+namespace BookingPlatform.Core.Interfaces.Repositories;
+
+public interface IRoomRepository
+{
+    Task CreateRoomAsync(Room room);
+    Task<Room?> GetRoomByIdAsync(Guid roomId);
+    Task<PaginatedResult<Room>> GetAvailbleRoomsByHotelIdAsync(Guid hotelId, int page, int size);
+    Task<bool> IsRoomAvailbleAsync(Guid roomId);
+    Task UpdateRoomAsync(Room room);
+    Task DeleteRoomByIdAsync(Guid roomId);
+
+}
+
