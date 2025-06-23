bae328ce8cc6b711a4ec2f4bbaeaa37002ef7fc5
ayabaara <ayabaara4@gmail.com>
2025-06-23 15:39:47 +0200

    add IImagelRepository interface for image data operations

---
+++ BookingPlatform.Core/Interfaces/Repositories/IImageRepository.cs
@@ -0,0 +1,15 @@
+﻿using BookingPlatform.Core.Entities;
+
+namespace BookingPlatform.Core.Interfaces.Repositories;
+
+public interface IImageRepository
+{
+    Task CreateAsync(Image image);
+    Task<Image?> GetImageByIdAsync(Guid imageId);
+    Task<List<Image>?> GetHotelImagesAsync(Guid hotelId);
+    Task<List<Image>?> GetRoomImagesAsync(Guid roomId);
+    Task UpdateImageAsync(Image image);
+    Task DeleteImageByIdAsync(Guid imageId);
+
+}
+
