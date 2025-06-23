b62e21612227a3b25859531904a0368db7507ff3
ayabaara <ayabaara4@gmail.com>
2025-06-23 15:37:45 +0200

    add IBookingRepository interface for booking data operations

---
+++ BookingPlatform.Core/Interfaces/Repositories/IBookingRepository.cs
@@ -0,0 +1,12 @@
+﻿using BookingPlatform.Core.Entities;
+
+namespace BookingPlatform.Core.Interfaces.Repositories;
+
+interface IBookingRepository
+{
+    Task CreateBookingAsync(Booking booking);
+    Task<List<Booking>?> GetBookingByUserIdAsync(Guid userId);
+    Task<List<Booking>?> GetRecentlyBookingByUserIdAsync(Guid userId);
+    
+}
+
