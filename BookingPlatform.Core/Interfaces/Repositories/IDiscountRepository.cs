082c2845dcb24a357436b803e3eba892eed3d39e
ayabaara <ayabaara4@gmail.com>
2025-06-23 15:38:50 +0200

    add IDiscountRepository interface for discount data operations

---
+++ BookingPlatform.Core/Interfaces/Repositories/IDiscountRepository.cs
@@ -0,0 +1,15 @@
+﻿using BookingPlatform.Core.Entities;
+
+namespace BookingPlatform.Core.Interfaces.Repositories;
+
+public interface IDiscountRepository
+{
+    Task CreateDiscountAsync(Discount discount);
+    Task<Discount?> GetDiscountByIdAsync(Guid discountId);
+    Task<List<Discount>> GetDiscountByRoomIdAsync(Guid roomId);
+    Task<PaginatedResult<Discount>> GetDiscountsAsync(int page, int size);
+    Task UpdateDiscountAsync(Discount discount);
+    Task DeleteDiscountByIdAsync(Guid discountId);
+
+}
+
