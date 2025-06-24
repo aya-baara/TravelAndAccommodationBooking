2730dc3baeeb508c09183778b2d16eb4beab2e0b
ayabaara <ayabaara4@gmail.com>
2025-06-24 14:06:42 +0200

    Romve Role property from Person to the user entity

--- BookingPlatform.Core/Entities/Person.cs
+++ BookingPlatform.Core/Entities/Person.cs
@@ -6,7 +6,6 @@ public class Person : BaseEntity
     public string LastName { get; set; }
     public int PhoneNumber { get; set; }
     public string Email { get; set; }
-    public Role Role { get; set; } = new Role();
     public DateTime DateOfBirth { get; set; }
 }
 
--- BookingPlatform.Core/Entities/User.cs
+++ BookingPlatform.Core/Entities/User.cs
@@ -2,6 +2,8 @@
 
 public class User : Person
 {
+    public Role Role { get; set; } = new Role();
+    public  Guid RoleId { get; set; }
     public string Password { get; set; } = string.Empty;
 }
 
