1f5bacfa60e2d697fb36d0cea845c33dfd4ebb32
ayabaara <ayabaara4@gmail.com>
2025-06-24 14:50:23 +0200

    add the dbcontext class and register it in program.cs

--- BookingPlatform.WebAPI/Program.cs
+++ BookingPlatform.WebAPI/Program.cs
@@ -1,8 +1,21 @@
+using BookingPlatform.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+using Microsoft.Extensions.Configuration;
+using System.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
+// Load config from appsettings.json
+var config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(config.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
@@ -14,28 +27,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
