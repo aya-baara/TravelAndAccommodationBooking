26fcaf396eb8240f67bfa3906a8fa3f5caf85d4b
ayabaara <ayabaara4@gmail.com>
2025-06-25 11:15:31 +0200

    add UserRepository implementation with basic user operations

---
+++ BookingPlatform.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,43 @@
+﻿using BookingPlatform.Core.Entities;
+using BookingPlatform.Core.Interfaces.Repositories;
+using BookingPlatform.Core.Interfaces.Services;
+using BookingPlatform.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookingPlatform.Infrastructure.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    private readonly AppDbContext _context;
+    private readonly IPasswordHasher _passwordHasher;
+
+    public UserRepository(AppDbContext context, IPasswordHasher passwordHasher)
+    {
+        _context = context;
+        _passwordHasher = passwordHasher;
+    }
+
+    public async Task<User?> AuthenticateUserAsync(string email, string password)
+    {
+        var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+        return user != null && _passwordHasher.VerifyPassword(password, user.Password) ? user : null;
+
+    }
+
+    public async Task CreateUserAsync(User user)
+    {
+        await _context.Users.AddAsync(user);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<bool> DoesUserExistAsync(string email)
+    {
+        return await _context.Users.AnyAsync(u => u.Email == email);
+    }
+
+    public async Task<User?> GetUserByIdAsync(Guid id)
+    {
+        return await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
+    }
+}
+
