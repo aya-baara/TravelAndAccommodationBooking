1d4bd1bf7d2248fa11570b5093532cc5085f397f
ayabaara <ayabaara4@gmail.com>
2025-06-25 13:00:06 +0200

    add BookingRepository implementation with booking operations

--- BookingPlatform.Core/Interfaces/Repositories/IBookingRepository.cs
+++ BookingPlatform.Core/Interfaces/Repositories/IBookingRepository.cs
@@ -5,8 +5,8 @@ namespace BookingPlatform.Core.Interfaces.Repositories;
 public interface IBookingRepository
 {
     Task CreateBookingAsync(Booking booking);
-    Task<List<Booking>?> GetBookingByUserIdAsync(Guid userId);
-    Task<List<Booking>?> GetRecentlyBookingByUserIdAsync(Guid userId);
+    Task<List<Booking>> GetBookingByUserIdAsync(Guid userId);
+    Task<List<Booking>> GetRecentlyBookingByUserIdAsync(Guid userId);
     
 }
 
---
+++ BookingPlatform.Infrastructure/Repositories/BookingRepository.cs
@@ -0,0 +1,33 @@
+﻿using BookingPlatform.Core.Entities;
+using BookingPlatform.Core.Interfaces.Repositories;
+using BookingPlatform.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookingPlatform.Infrastructure.Repositories;
+
+public class BookingRepository : IBookingRepository
+{
+    private readonly AppDbContext _context;
+    public BookingRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task CreateBookingAsync(Booking booking)
+    {
+         _context.Bookings.Add(booking);
+        await _context.SaveChangesAsync();
+    }
+
+    public Task<List<Booking>> GetBookingByUserIdAsync(Guid userId)
+    {
+        return _context.Bookings.Where(b => b.UserId == userId).ToListAsync();
+    }
+
+    public Task<List<Booking>> GetRecentlyBookingByUserIdAsync(Guid userId)
+    {
+        return _context.Bookings.Where(b => b.UserId == userId)
+            .OrderByDescending(b => b.BookingDate).Take(3).ToListAsync();
+    }
+}
+
