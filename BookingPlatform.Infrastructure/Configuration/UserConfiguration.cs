1de1e8cc32d475758e7b86a8819ec844af29fe43
ayabaara <ayabaara4@gmail.com>
2025-06-25 10:07:48 +0200

    Add  Unique Constraint to email and update the migration

--- BookingPlatform.Infrastructure/Configuration/UserConfiguration.cs
+++ BookingPlatform.Infrastructure/Configuration/UserConfiguration.cs
@@ -11,9 +11,10 @@ public class UserConfiguration : IEntityTypeConfiguration<User>
         builder.HasKey(u => u.Id);
 
         builder.Property(u => u.Email).IsRequired();
+        builder.HasIndex(u => u.Email).IsUnique();
         builder.Property(u => u.Password).IsRequired();
-
-         builder.HasOne(u => u.Role)
+       
+        builder.HasOne(u => u.Role)
             .WithMany(r => r.Users)
             .HasForeignKey(u => u.RoleId)
             .OnDelete(DeleteBehavior.Restrict);
