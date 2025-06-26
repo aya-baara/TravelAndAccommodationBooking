fc8f963b3a530b14ba73241b80bfeadd7a0f5763
ayabaara <ayabaara4@gmail.com>
2025-06-26 10:33:48 +0200

    add InvoiceRepository implementation with Invoice operations

---
+++ BookingPlatform.Infrastructure/Repositories/InvoiceRepository.cs
@@ -0,0 +1,48 @@
+﻿using BookingPlatform.Core.Entities;
+using BookingPlatform.Core.Interfaces.Repositories;
+using BookingPlatform.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookingPlatform.Infrastructure.Repositories;
+
+public class InvoiceRepository : IInvoiceRopsitory
+{
+    private readonly AppDbContext _context;
+    public InvoiceRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task CreateInvoiceAsync(Invoice invoice)
+    {
+        _context.Invoices.Add(invoice);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteInvoiceAsync(Guid invoiceId)
+    {
+        var invoice = await GetInvoiceByIdAsync(invoiceId);
+        if(invoice != null)
+        {
+            _context.Invoices.Remove(invoice);
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    public async Task<Invoice?> GetInvoiceByBookingIdAsync(Guid bookingId)
+    {
+        return await _context.Invoices.FirstOrDefaultAsync(i => i.BookingId == bookingId);
+    }
+
+    public async Task<Invoice?> GetInvoiceByIdAsync(Guid invoiceId)
+    {
+        return await _context.Invoices.FirstOrDefaultAsync(i => i.Id == invoiceId);
+    }
+
+    public async Task UpdateInvoiceAsync(Invoice invoice)
+    {
+        _context.Invoices.Update(invoice);
+        await _context.SaveChangesAsync();
+    }
+}
+
