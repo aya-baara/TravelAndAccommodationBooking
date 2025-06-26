48691ac0771fd751e73e7d267f305bc3abf11221
ayabaara <ayabaara4@gmail.com>
2025-06-26 11:46:52 +0200

    add SmtpSettings to congiguration & register service

--- BookingPlatform.WebAPI/Program.cs
+++ BookingPlatform.WebAPI/Program.cs
@@ -1,6 +1,7 @@
 using BookingPlatform.Core.Interfaces.Services;
 using BookingPlatform.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using BookingPlatform.Infrastructure.Services;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,12 +13,21 @@ var config = new ConfigurationBuilder()
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
 builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
 
+
+
+
+builder.Services.Configure<SmtpSettings>(
+    builder.Configuration.GetSection("SmtpSettings"));
+
+builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
+builder.Services.AddScoped<IEmailService, EmailService>();
 
 
 var app = builder.Build();
@@ -33,3 +43,4 @@ app.UseHttpsRedirection();
 
 
 app.Run();
+
