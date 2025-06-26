0a2e12dc725af6fc1517292a563af8733cdac420
ayabaara <ayabaara4@gmail.com>
2025-06-26 10:35:07 +0200

    add RoleRepository implementation with Role operations

---
+++ BookingPlatform.Infrastructure/Repositories/RoleRepository.cs
@@ -0,0 +1,43 @@
+﻿using BookingPlatform.Core.Entities;
+using BookingPlatform.Core.Enums;
+using BookingPlatform.Core.Interfaces.Repositories;
+using BookingPlatform.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookingPlatform.Infrastructure.Repositories;
+
+public class RoleRepository : IRoleRepository
+{
+    private readonly AppDbContext _context;
+    public RoleRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task CreateRoleAsync(Role role)
+    {
+        _context.Roles.Add(role);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteRoleAsync(Guid roleId)
+    {
+        var role = await _context.Roles.FirstOrDefaultAsync(r => r.Id == roleId);
+        if(role != null)
+        {
+            _context.Roles.Remove(role);
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    public async Task<List<User>> GetAdmins()
+    {
+        return await _context.Users.Where(u => u.Role.Name == UserRole.Admin).ToListAsync();
+    }
+
+    public async Task<List<Role>> GetRolesAsync()
+    {
+        return await _context.Roles.ToListAsync();
+    }
+}
+
