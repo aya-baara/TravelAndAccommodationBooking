84952291a65b02552b8a6e245ee1cd77219ca2e3
ayabaara <ayabaara4@gmail.com>
2025-06-29 14:06:31 +0200

    Add create owner command , handler & dto

---
+++ BookingPlatform.Application/Mapping/OwnerProfile .cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using BookingPlatform.Application.Owners.Commands;
+using BookingPlatform.Application.Owners.Dtos;
+using BookingPlatform.Core.Entities;
+
+namespace BookingPlatform.Application.Mapping;
+
+public class OwnerProfile : Profile
+{
+    public OwnerProfile()
+    {
+        CreateMap<Owner, OwnerResponseDto>();
+        CreateMap<CreateOwnerCommand, Owner>();
+    }
+}
+
---
+++ BookingPlatform.Application/Owners/Commands/CreateOwnerCommand.cs
@@ -0,0 +1,13 @@
+﻿using BookingPlatform.Application.Owners.Dtos;
+using MediatR;
+
+namespace BookingPlatform.Application.Owners.Commands;
+
+public class CreateOwnerCommand : IRequest<OwnerResponseDto>
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int PhoneNumber { get; set; }
+    public string Email { get; set; }
+    public DateTime DateOfBirth { get; set; }
+}
