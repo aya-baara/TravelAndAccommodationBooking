c63d763dd6cc8c12ae3c548686a05f5abf96fe6d
ayabaara <ayabaara4@gmail.com>
2025-06-29 17:15:16 +0200

    Add ImageDto and profile

---
+++ BookingPlatform.Application/Mapping/ImageProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using BookingPlatform.Application.Images.Dtos;
+using BookingPlatform.Core.Entities;
+
+namespace BookingPlatform.Application.Mapping;
+
+public class ImageProfile :Profile
+{
+    public ImageProfile()
+    {
+        CreateMap<Image, ImageResponseDto>();
+    }
+}
+
