60b329a442e7c6cf7fe843367f8da90e41852866
ayabaara <ayabaara4@gmail.com>
2025-06-29 15:21:59 +0200

    add  crateHotel commands & it's handler,profile

---
+++ BookingPlatform.Application/Hotels/Commands/Create/CreateHotelCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using BookingPlatform.Application.Hotels.Dtos;
+using BookingPlatform.Core.Entities;
+using BookingPlatform.Core.Interfaces.Repositories;
+using MediatR;
+
+namespace BookingPlatform.Application.Hotels.Commands.Create;
+
+public class CreateHotelCommandHandler : IRequestHandler<CreateHotelCommand, HotelResponseDto>
+{
+    private readonly IHotelRepository _hotelRepository;
+    private readonly IMapper _mapper;
+    public CreateHotelCommandHandler(IHotelRepository hotelRepository, IMapper mapper)
+    {
+        _hotelRepository = hotelRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<HotelResponseDto> Handle(CreateHotelCommand request, CancellationToken cancellationToken)
+    {
+        var hotel = _mapper.Map<Hotel>(request);
+        var created =await  _hotelRepository.CreateHotelAsync(hotel);
+        return _mapper.Map<HotelResponseDto>(created);
+    }
+}
+
---
+++ BookingPlatform.Application/Mapping/HotelProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using BookingPlatform.Application.Hotels.Commands.Create;
+using BookingPlatform.Application.Hotels.Dtos;
+using BookingPlatform.Core.Entities;
+
+namespace BookingPlatform.Application.Mapping;
+
+public class HotelProfile : Profile
+{
+    public HotelProfile()
+    {
+        CreateMap<CreateHotelCommand, Hotel>();
+        CreateMap<Hotel, HotelResponseDto>();
+    }
+}
+
