3a9ac15111fcf8e8905175aeaffa99f363810694
ayabaara <ayabaara4@gmail.com>
2025-06-29 17:14:16 +0200

    Add reviwDto and profile

---
+++ BookingPlatform.Application/Mapping/ReviewProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using BookingPlatform.Application.Reviews.Dtos;
+using BookingPlatform.Core.Entities;
+
+namespace BookingPlatform.Application.Mapping;
+
+public class ReviewProfile : Profile
+{
+    public ReviewProfile()
+    {
+        CreateMap<Review, ReviewResponseDto>();
+    }
+}
+
---
+++ BookingPlatform.Application/Reviews/Dtos/ReviewResponseDto.cs
@@ -0,0 +1,11 @@
+﻿using BookingPlatform.Core.Entities;
+
+namespace BookingPlatform.Application.Reviews.Dtos;
+
+public class ReviewResponseDto
+{
+    public User User { get; set; }
+    public string Comment { get; set; }
+    public double Rate { get; set; }
+}
+
