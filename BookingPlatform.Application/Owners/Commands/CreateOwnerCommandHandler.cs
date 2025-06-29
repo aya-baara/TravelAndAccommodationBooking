4ffb19d52d21635921f6194c015dfd6f01d937fb
ayabaara <ayabaara4@gmail.com>
2025-06-29 14:07:51 +0200

    Add GetOwnerById query and it's handler

---
+++ BookingPlatform.Application/Owners/Commands/CreateOwnerCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using BookingPlatform.Application.Owners.Commands;
+using BookingPlatform.Application.Owners.Dtos;
+using BookingPlatform.Core.Entities;
+using BookingPlatform.Core.Interfaces.Repositories;
+using MediatR;
+
+namespace BookingPlatform.Application.Owners.Handlers.CommandHandlers;
+
+public class CreateOwnerCommandHandler : IRequestHandler<CreateOwnerCommand, OwnerResponseDto>
+{
+    private readonly IMapper _mapper;
+    private readonly IOwnerRepository _ownerRepository;
+
+    public CreateOwnerCommandHandler(IMapper mapper, IOwnerRepository ownerRepository)
+    {
+        _mapper = mapper;
+        _ownerRepository = ownerRepository;
+    }
+
+    public async Task<OwnerResponseDto> Handle(CreateOwnerCommand request, CancellationToken cancellationToken)
+    {
+        var ownerEntity = _mapper.Map<Owner>(request);
+        var createdOwner = await _ownerRepository.CreateOwnerAsync(ownerEntity);
+        return _mapper.Map<OwnerResponseDto>(createdOwner);
+    }
+}
+
---
+++ BookingPlatform.Application/Owners/Queries/GetOwnerById/GetOwnerByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using BookingPlatform.Application.Owners.Dtos;
+using MediatR;
+
+namespace BookingPlatform.Application.Owners.Queries.GetOwnerById;
+
+public class GetOwnerByIdQuery :IRequest<OwnerResponseDto>
+{
+    public Guid Id { get; set; }
+}
+
