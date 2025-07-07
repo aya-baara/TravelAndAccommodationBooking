d65c5e5ce8015ac13006d180d56cfbef07e4202f
ayabaara <ayabaara4@gmail.com>
2025-07-07 18:42:44 +0200

    Adding Roles' dtos

---
+++ BookingPlatform.Application/Dtos/Roles/CreateRoleDto.cs
@@ -0,0 +1,10 @@
+﻿using BookingPlatform.Core.Enums;
+
+namespace BookingPlatform.Application.Dtos.Roles;
+
+public class CreateRoleDto
+{
+    public UserRole Name { get; set; }
+    public string Description { get; set; } = string.Empty;
+}
+
---
+++ BookingPlatform.Application/Dtos/Roles/RoleResponseDto.cs
@@ -0,0 +1,11 @@
+﻿using BookingPlatform.Core.Enums;
+
+namespace BookingPlatform.Application.Dtos.Roles;
+
+public class RoleResponseDto
+{
+    public Guid Id { get; set; }
+    public UserRole Name { get; set; }
+    public string Description { get; set; } = string.Empty;
+}
+
