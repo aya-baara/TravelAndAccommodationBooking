0472a191e887c7decc6863ae99bbd7a1151fb248
ayabaara <ayabaara4@gmail.com>
2025-07-07 18:46:50 +0200

    Adding Reviews dtos

--- BookingPlatform.Application/Dtos/Reviews/UpdateReviewDto.cs
+++ BookingPlatform.Application/Dtos/Reviews/UpdateReviewDto.cs
@@ -1,10 +1,10 @@
 ﻿namespace BookingPlatform.Application.Dtos.Reviews;
-
-public class ReviewResponseDto
+public class UpdateReviewDto
 {
     public Guid Id { get; set; }
     public Guid UserId { get; set; }
     public string Comment { get; set; }
     public double Rate { get; set; }
+    public Guid HotelId { get; set; }
 }
 
