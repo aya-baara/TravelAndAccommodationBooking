9c4d577d0d68d370055140101425d31f7145653a
ayabaara <ayabaara4@gmail.com>
2025-07-07 18:45:35 +0200

    Adding Discount dtos

---
+++ BookingPlatform.Application/Dtos/Discounts/DiscountResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BookingPlatform.Application.Dtos.Discounts;
+
+public class DiscountResponseDto
+{
+    public Guid Id { get; set; }
+    public Guid RoomId { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public decimal Percentage { get; set; }
+}
