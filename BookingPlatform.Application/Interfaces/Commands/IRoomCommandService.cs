0d7a42f875156a4152b869c115965b4372efdbf8
ayabaara <ayabaara4@gmail.com>
2025-07-07 19:12:02 +0200

    add IRoomCommandService interface for room command operations

---
+++ BookingPlatform.Application/Interfaces/Commands/IRoomCommandService.cs
@@ -0,0 +1,11 @@
+﻿using BookingPlatform.Application.Dtos.Rooms;
+
+namespace BookingPlatform.Application.Interfaces.Commands;
+
+public interface IRoomCommandService
+{
+    Task<RoomResponseDto> CreateRoomAsync(CreateRoomDto dto, CancellationToken cancellationToken);
+    Task DeleteRoomByIdAsync(Guid id, CancellationToken cancellationToken);
+    Task UpdateRoomAsync(UpdateRoomDto dto, CancellationToken cancellationToken);
+}
+
