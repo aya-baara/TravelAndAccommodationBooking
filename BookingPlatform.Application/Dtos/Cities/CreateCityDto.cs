3206a708dfb66a4bfc706a98d7a91288f079a236
ayabaara <ayabaara4@gmail.com>
2025-07-07 18:44:55 +0200

    Adding City dtos

---
+++ BookingPlatform.Application/Dtos/Cities/CreateCityDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BookingPlatform.Application.Dtos.Cities;
+
+public class CreateCityDto
+{
+    public string Name { get; set; }
+    public string Country { get; set; }
+    public string PostOffice { get; set; }
+    public string Description { get; set; }
+}
+
