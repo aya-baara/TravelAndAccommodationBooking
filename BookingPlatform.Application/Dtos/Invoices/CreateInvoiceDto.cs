07f25b6cdfa1faa9107e21fdd4d6fdbbe7400c0f
ayabaara <ayabaara4@gmail.com>
2025-07-07 18:44:02 +0200

    Adding Invoices dtos

---
+++ BookingPlatform.Application/Dtos/Invoices/CreateInvoiceDto.cs
@@ -0,0 +1,14 @@
+﻿using BookingPlatform.Core.Enums;
+
+namespace BookingPlatform.Application.Dtos.Invoices;
+
+public class CreateInvoiceDto
+{
+    public Guid BookingId { get; set; }
+    public DateTime InvoiceDate { get; set; } = DateTime.UtcNow;
+    public decimal TotalAmount { get; set; }
+    public PaymentType PaymentMethod { get; set; }
+    public bool IsPaid { get; set; }
+    public string? Notes { get; set; }
+}
+
---
+++ BookingPlatform.Application/Dtos/Invoices/InvoiceResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using BookingPlatform.Core.Enums;
+
+namespace BookingPlatform.Application.Dtos.Invoices;
+
+public class InvoiceResponseDto
+{
+    public Guid Id { get; set; }
+    public Guid BookingId { get; set; }
+    public DateTime InvoiceDate { get; set; } = DateTime.UtcNow;
+    public decimal TotalAmount { get; set; }
+    public PaymentType PaymentMethod { get; set; }
+    public bool IsPaid { get; set; }
+    public string? Notes { get; set; }
+}
+
