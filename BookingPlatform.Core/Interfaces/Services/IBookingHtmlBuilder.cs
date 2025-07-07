adb44cfd45e5a0d23a74fc1a6a52f3ecc935e10a
ayabaara <ayabaara4@gmail.com>
2025-07-07 18:58:28 +0200

    Add helper Interfaces for the booking logic and invoice print

---
+++ BookingPlatform.Core/Interfaces/Services/IBookingHtmlBuilder.cs
@@ -0,0 +1,9 @@
+﻿using BookingPlatform.Core.Entities;
+
+namespace BookingPlatform.Core.Interfaces.Services;
+
+public interface IBookingHtmlBuilder
+{
+    string BuildConfirmationHtml(Booking booking);
+}
+
