b9cc6181c04aa764dc767eebebffe697b559373c
ayabaara <ayabaara4@gmail.com>
2025-07-07 19:09:18 +0200

    add IInvoiceCommandService interface for invoice command operations

---
+++ BookingPlatform.Application/Interfaces/Commands/IInvoiceCommandService.cs
@@ -0,0 +1,11 @@
+﻿using BookingPlatform.Application.Dtos.Invoices;
+
+namespace BookingPlatform.Application.Interfaces.Commands;
+
+public interface IInvoiceCommandService
+{
+    Task<InvoiceResponseDto> CreateInvoiceAsync(CreateInvoiceDto dto, CancellationToken cancellationToken);
+    Task DeleteInvoiceAsync(Guid id, CancellationToken cancellationToken);
+    Task UpdateInvoiceAsync(UpdateInvoiceDto dto, CancellationToken cancellationToken);
+}
+
