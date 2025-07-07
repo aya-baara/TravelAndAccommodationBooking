3daeac01b4587b2f5fb46fb5a0130ce4e78712a1
ayabaara <ayabaara4@gmail.com>
2025-07-07 19:04:20 +0200

    add IBookingCommandService interface for booking command operations

---
+++ BookingPlatform.Application/Interfaces/Commands/IBookingCommandService.cs
@@ -0,0 +1,11 @@
+﻿using BookingPlatform.Application.Dtos.Bookings;
+
+namespace BookingPlatform.Application.Interfaces.Commands;
+
+public interface IBookingCommandService
+{
+    Task<BookingResponseDto> createBookingAsync(CreateBookingDto dto, CancellationToken cancellationToken);
+    Task DeleteBookingAsync(Guid id, CancellationToken cancellationToken);
+    Task UpdateBookingAsync(UpdateBookingDto dto, CancellationToken cancellationToken);
+}
+
