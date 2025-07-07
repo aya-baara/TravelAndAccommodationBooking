21356c14183b798a259ecd855d48d4be5cfec40b
ayabaara <ayabaara4@gmail.com>
2025-07-07 19:18:36 +0200

    add IHotelQueryService interface for hotel query operations

--- BookingPlatform.Application/Interfaces/Queries/IHotelQueryService.cs
+++ BookingPlatform.Application/Interfaces/Queries/IHotelQueryService.cs
@@ -5,5 +5,8 @@ namespace BookingPlatform.Application.Interfaces.Queries;
 public interface IHotelQueryService
 {
     Task<HotelDetailsDto> GetHotelDetailsByIdAsync(Guid id, CancellationToken cancellationToken);
+    Task<List<FeaturedHotelDto>> GetFeaturedDealsAsync(int count, CancellationToken cancellationToken);
+    Task<PaginatedResult<HotelSearchDto>> SearchHotelsAsync(HotelSearchRequest request, CancellationToken ct);
+    Task<PaginatedResult<HotelManagementDto>> SearchHotelsAdminAsync(HotelAdminSearchRequest request, CancellationToken ct);
 }
 
