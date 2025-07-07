9560d80bcdb8d66456eaff07b63642c19e2706a4
ayabaara <ayabaara4@gmail.com>
2025-07-07 19:00:41 +0200

    Add Sieve configuration to help in the search

---
+++ BookingPlatform.Application/SieveConfigurations/CompositeSieveConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Sieve.Services;
+
+namespace BookingPlatform.Application.SieveConfigurations;
+
+public class CompositeSieveConfiguration : ISieveConfiguration
+{
+    private readonly IEnumerable<ISieveCustomConfiguration> _customConfigs;
+
+    public CompositeSieveConfiguration(IEnumerable<ISieveCustomConfiguration> customConfigs)
+    {
+        _customConfigs = customConfigs;
+    }
+
+    public void Configure(SievePropertyMapper mapper)
+    {
+        foreach (var config in _customConfigs)
+        {
+            config.Apply(mapper);
+        }
+    }
+}
