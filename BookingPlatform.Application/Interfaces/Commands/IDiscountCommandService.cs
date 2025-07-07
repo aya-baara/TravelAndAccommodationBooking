cba2cc56a312c2612b66e5a34eb0e1c863c3cf9a
ayabaara <ayabaara4@gmail.com>
2025-07-07 19:06:52 +0200

    add IDiscountCommandService interface for city command operations

---
+++ BookingPlatform.Application/Interfaces/Commands/IDiscountCommandService.cs
@@ -0,0 +1,11 @@
+﻿using BookingPlatform.Application.Dtos.Discounts;
+
+namespace BookingPlatform.Application.Interfaces.Commands;
+
+public interface IDiscountCommandService
+{
+    Task<DiscountResponseDto> CreateDiscountAsync(CreateDiscountDto dto, CancellationToken cancellationToken);
+    Task UpdateDiscountAsync(UpdateDiscountDto dto, CancellationToken cancellationToken);
+    Task DeleteDiscountAsync(Guid id, CancellationToken cancellationToken);
+}
+
