83f24f0223c0e8a36554f29cdb96407c4d03c8e3
ayabaara <ayabaara4@gmail.com>
2025-07-07 19:19:43 +0200

    add IBookingQueryService interface for booking query operations

---
+++ BookingPlatform.Application/Interfaces/Queries/IBookingQueryService.cs
@@ -0,0 +1,11 @@
+﻿using BookingPlatform.Application.Dtos.Bookings;
+using BookingPlatform.Application.Dtos.Hotels;
+
+namespace BookingPlatform.Application.Interfaces.Queries;
+
+public interface IBookingQueryService
+{
+    Task<BookingResponseDto> GetBookingByIdAsync(Guid id, CancellationToken cancellationToken);
+    Task<List<RecentHotelDto>> GetRecentHotelsForUserAsync(Guid userId, int count, CancellationToken cancellationToken);
+}
+
