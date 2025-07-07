064691c178d9f8d07d0603598419898f14eabe2e
ayabaara <ayabaara4@gmail.com>
2025-07-07 19:05:49 +0200

    add ICityCommandService interface for city command operations

---
+++ BookingPlatform.Application/Interfaces/Commands/ICityCommandService.cs
@@ -0,0 +1,11 @@
+﻿using BookingPlatform.Application.Dtos.Cities;
+
+namespace BookingPlatform.Application.Interfaces.Commands;
+
+public interface ICityCommandService
+{
+    Task<CityResponseDto> CreateCityAsync(CreateCityDto dto, CancellationToken cancellationToken);
+    Task UpdateCityAsync(UpdateCityDto dto, CancellationToken cancellationToken);
+    Task DeleteCityAsync(Guid id, CancellationToken cancellationToken);
+}
+
