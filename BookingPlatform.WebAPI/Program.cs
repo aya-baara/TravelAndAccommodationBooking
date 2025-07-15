0774a1803694f278ccc70bac947643c96d86ba16
ayabaara <ayabaara4@gmail.com>
2025-07-15 16:26:35 +0200

    register services

--- BookingPlatform.WebAPI/Program.cs
+++ BookingPlatform.WebAPI/Program.cs
@@ -1,4 +1,3 @@
-using BookingPlatform.Core.Interfaces.Services;
 using BookingPlatform.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using BookingPlatform.Infrastructure.Services;
@@ -8,28 +7,17 @@ using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using BookingPlatform.Core.Interfaces.Auth;
 using Serilog;
-using BookingPlatform.Application.Services.Helpers;
-using BookingPlatform.Application.SieveConfigurations;
-using Sieve.Services;
-using BookingPlatform.Core.Interfaces.Repositories;
-using BookingPlatform.Infrastructure.Repositories;
-using BookingPlatform.Core.Interfaces;
-using BookingPlatform.Infrastructure;
-using BookingPlatform.Application.Interfaces.Commands;
-using BookingPlatform.Application.Services.Commands;
-using BookingPlatform.Application.Interfaces.Queries;
-using BookingPlatform.Application.Services.Queries;
-
+using BookingPlatform.WebAPI.Middlewares;
+using FluentValidation.AspNetCore;
+using FluentValidation;
+using BookingPlatform.WebAPI.Extensions;
+using System.Text.Json.Serialization;
+using BookingPlatform.WebAPI.Helpers;
+using BookingPlatform.Application.Mapping;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
-// Load config from appsettings.json
-var config = new ConfigurationBuilder()
-    .AddJsonFile("appsettings.json")
-    .Build();
-
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 
 Log.Logger = new LoggerConfiguration()
     .WriteTo.Console()
@@ -38,10 +26,12 @@ Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Information()
     .CreateLogger();
 
+builder.Services.AddOpenApi();
 builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
 builder.Services.AddAuthentication(k =>
 {
     k.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
@@ -61,93 +51,80 @@ builder.Services.AddAuthentication(k =>
         IssuerSigningKey = new SymmetricSecurityKey(key)
     };
 });
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
 builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("JWTToken"));
 builder.Services.AddSingleton<ITokenGenerator, JwtTokenGenerator>();
-
 builder.Services.AddAuthorization();
-
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
 builder.Services.Configure<SmtpSettings>(builder.Configuration.GetSection("SmtpSettings"));
-builder.Services.AddScoped<IEmailService, EmailService>();
-builder.Services.AddSingleton<IPdfService, PdfGeneratorService>();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()); // optional
+        options.JsonSerializerOptions.Converters.Add(new DateOnlyJsonConverter());
+    });
+builder.Services.AddSwaggerGen(options =>
+{
+    // Add Swagger document info
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "MinimalAPI",
+        Version = "v1"
+    });
 
+    // Include XML comments
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    options.IncludeXmlComments(xmlPath);
 
-builder.Services.AddScoped<ISieveProcessor, SieveProcessor>();
-// Register all custom Sieve configurations
-builder.Services.AddScoped<ISieveCustomConfiguration, HotelSieveConfiguration>();
-builder.Services.AddScoped<ISieveCustomConfiguration, CitySieveConfiguration>();
-builder.Services.AddScoped<ISieveCustomConfiguration, RoomSieveConfiguration>();
-builder.Services.AddScoped<ISieveCustomConfiguration, HotelManagementSieveConfiguration>();
+    // JWT Authentication setup for Swagger
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Enter 'Bearer' [space] and then your valid token.\n\nExample: Bearer eyJhbGciOiJIUzI1NiIsInR..."
+    });
 
-// Register the composite config as the only ISieveConfiguration
-builder.Services.AddScoped<ISieveConfiguration, CompositeSieveConfiguration>();
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
+builder.Services.AddAutoMapper(typeof(OwnerProfile).Assembly);
+builder.Services.AddFluentValidationAutoValidation(); // Auto model validation
+builder.Services.AddValidatorsFromAssemblyContaining<Program>(); // Scan validators
 
-builder.Services.AddScoped<IBookingNotificationService, BookingNotificationService>();
-builder.Services.AddScoped<IBookingCreationService, BookingCreationService>();
-builder.Services.AddScoped<IBookingHtmlBuilder, BookingHtmlBuilder>();
-builder.Services.AddScoped<IInvoiceHtmlBuilder, InvoiceHtmlBuilder>();
-
-builder.Services.AddScoped<IBookingRepository, BookingRepository>();
-builder.Services.AddScoped<ICityRepository, CityRepository>();
-builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
-builder.Services.AddScoped<IHotelRepository, HotelRepository>();
-builder.Services.AddScoped<IImageRepository, ImageRepository>();
-builder.Services.AddScoped<IInvoiceRopsitory, InvoiceRepository>();
-builder.Services.AddScoped<IOwnerRepository, OwnerRepository>();
-builder.Services.AddScoped<IReviewRepository, ReviewRepository>();
-builder.Services.AddScoped<IRoleRepository, RoleRepository>();
-builder.Services.AddScoped<IRoomRepository, RoomRepository>();
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-
-builder.Services.AddScoped<IBookingCommandService, BookingCommandService>();
-builder.Services.AddScoped<IBookingQueryService, IBookingQueryService>();
-
-builder.Services.AddScoped<ICityCommandService, CityCommandService>();
-builder.Services.AddScoped<ICityQueryService, CityQueryService>();
-
-builder.Services.AddScoped<IDiscountCommandService, DiscountCommandService>();
-builder.Services.AddScoped<IDiscountQueryService, DiscountQueryService>();
-
-builder.Services.AddScoped<IHotelCommandService, HotelCommandService>();
-builder.Services.AddScoped<IHotelQueryService, HotelQueryService>();
-
-builder.Services.AddScoped<IImageCommandService, ImageCommandService>();
-builder.Services.AddScoped<IImageQueryService, ImageQueryService>();
-
-builder.Services.AddScoped<IInvoiceCommandService, InvoiceCommandService>();
-builder.Services.AddScoped<IInvoiceQueryService, InvoiceQueryService>();
-
-builder.Services.AddScoped<IOwnerCommandService, OwnerCommandService>();
-builder.Services.AddScoped<IOwnerQueryService, OwnerQueryService>();
-
-builder.Services.AddScoped<IReviewCommandService, ReviewCommandService>();
-builder.Services.AddScoped<IReviewQueryService, ReviewQueryService>();
-
-builder.Services.AddScoped<IRoomCommandService, RoomCommandService>();
-builder.Services.AddScoped<IRoomQueryService, RoomQueryService>();
-
-builder.Services.AddScoped<IUserCommandService, UserCommandService>();
-builder.Services.AddScoped<IUserQueryService, UserQueryService>();
-
-builder.Services.AddScoped<IRoleCommandService, RoleCommandService>();
-builder.Services.AddScoped<IRoleQueryService, RoleQueryService>();
-
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices();
 
 builder.Host.UseSerilog();
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseHttpsRedirection();
+
+app.UseMiddleware<ExceptionHandlingMiddleware>();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
-
-
-
+app.MapControllers();
 app.Run();
 
